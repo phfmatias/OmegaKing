4656c3d596ebcf6da9a9c1d5670eadbdae171cc1
phfmatias <pedrohfmatias@gmail.com>
2026-03-12 12:47:41 -0300

    Update createGaussianInput calls to include all MK_Molecule variants in optimize method

--- APP/optimize.py
+++ APP/optimize.py
@@ -96,7 +96,7 @@ class Optimizer():
                     pass
             
             elif x1 not in omega_values:
-                files_x1 = CI.createGaussianInput(x1, self.MK_Molecule, self.target)
+                files_x1 = CI.createGaussianInput(x1, self.MK_MoleculeNeutral, self.MK_MoleculeCation, self.MK_MoleculeAnion, self.target)
                 RQM.extend(files_x1)
                 omega_values.append(x1)
 
@@ -110,7 +110,7 @@ class Optimizer():
                     pass
 
             elif x2 not in omega_values:
-                files_x2 = CI.createGaussianInput(x2, self.MK_Molecule, self.target)
+                files_x2 = CI.createGaussianInput(x2, self.MK_MoleculeNeutral, self.MK_MoleculeCation, self.MK_MoleculeAnion, self.target)
                 RQM.extend(files_x2)
                 omega_values.append(x2)
             
